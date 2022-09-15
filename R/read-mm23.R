dca8db7646a483621e9146f455092ee5ff1392d8
David Lee <david@lee269.co.uk>
2022-09-15 21:00:47 +0100

    test2

---
+++ R/read-mm23.R
@@ -0,0 +1,55 @@
+library(here)
+library(tidyverse)
+library(tidyr)
+library(lubridate)
+library(ggplot2)
+library(janitor)
+
+# https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv
+# read_csv("https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv")
+# Not sure why this code seems to pull in a different version of mm23 than manually downloading.
+# x <- read.csv("https://www.ons.gov.uk/file?uri=/economy/inflationandpriceindices/datasets/consumerpriceindices/current/mm23.csv")
+# write_csv(x, here("data", "raw", "mm23.csv"))
+
+
+metadata <- read_csv(here("data", "raw", "mm23.csv"), n_max = 6) %>% 
+            t() %>% 
+            as_tibble(rownames = "series") %>% 
+            setNames( .[1,]) %>% 
+            # rename(series = Title) %>%
+            filter(Title != "Title") %>% 
+            clean_names() %>% 
+            relocate(cdid)
+  
+            # pivot_longer(cols = 2:ncol(.), names_to = "series")
+
+saveRDS(metadata, here("data", "tidy", "metadata.rds"))
+
+
+mm23_month <- read_csv(here("data", "raw", "mm23.csv"), skip = 1) %>% 
+        filter(nchar(CDID) == 8) %>% 
+        mutate(CDID = ym(CDID)) %>%
+        rename(date = CDID) %>% 
+        pivot_longer(cols = 2:ncol(.), names_to = "cdid") %>% 
+        mutate(value = as.numeric(value))
+
+saveRDS(mm23_month, here("data", "tidy", "mm23_month.rds"))  
+
+
+mm23_quarter <- read_csv(here("data", "raw", "mm23.csv"), skip = 1) %>% 
+  filter(nchar(CDID) == 7 & CDID != "PreUnit") %>% 
+  mutate(CDID = yq(CDID)) %>% 
+  rename(date = CDID) %>% 
+  pivot_longer(cols = 2:ncol(.), names_to = "cdid") %>% 
+  mutate(value = as.numeric(value))
+
+saveRDS(mm23_quarter, here("data", "tidy", "mm23_quarter.rds"))  
+
+mm23_year <- read_csv(here("data", "raw", "mm23.csv"), skip = 1) %>% 
+  filter(nchar(CDID) == 4 & CDID != "Unit") %>% 
+  mutate(CDID = as.numeric(CDID)) %>% 
+  rename(date = CDID) %>% 
+  pivot_longer(cols = 2:ncol(.), names_to = "cdid") %>% 
+  mutate(value = as.numeric(value))
+
+saveRDS(mm23_year, here("data", "tidy", "mm23_year.rds"))  
---
+++ R/wrangle-cdids.R
@@ -0,0 +1,110 @@
+library(here)
+library(tidyverse)
+
+metadata <- readRDS(here("data",  "tidy", "metadata.rds"))
+
+# CPIH annual rates ------------------------------------------------------------
+# Extract series and add a level flag to show hierarchy
+
+
+cpih_ann1 <- metadata %>% 
+  filter(str_detect(title, "CPIH ANNUAL RATE [0-9][0-9] ?:")) %>% 
+  mutate(title = str_remove(title, "CPIH ANNUAL RATE ")) %>% 
+  mutate(level = 1)
+
+cpih_ann2 <- metadata %>% 
+  filter(str_detect(title, "CPIH ANNUAL RATE [0-9][0-9]\\.[0-9] ?:")) %>% 
+  mutate(title = str_remove(title, "CPIH ANNUAL RATE ")) %>% 
+  mutate(level = 2)
+
+cpih_ann3 <- metadata %>% 
+  filter(str_detect(title, "CPIH ANNUAL RATE [0-9][0-9]\\.[0-9].[0-9] ?:")) %>% 
+  mutate(title = str_remove(title, "CPIH ANNUAL RATE ")) %>% 
+  mutate(level = 3)
+
+
+cpih_ann4 <- metadata %>% 
+  filter(str_detect(title, "CPIH ANNUAL RATE [0-9][0-9]\\.[0-9].[0-9].[0-9]")) %>% 
+  mutate(title = str_remove(title, "CPIH ANNUAL RATE ")) %>% 
+  mutate(level = 4)
+
+
+cpih_ann <- bind_rows(list(cpih_ann1, cpih_ann2, cpih_ann3, cpih_ann4)) %>% 
+            mutate(title = str_remove(title, " 2015=100")) %>% 
+            arrange(title)
+
+saveRDS(cpih_ann, here("data", "tidy", "cpih_ann_rate_cdids.rds"))
+
+
+# CPIH monthly rates -----------------------------------------------------------
+
+cpih_mth1 <- metadata %>% 
+  filter(str_detect(title, "CPIH MONTHLY RATE [0-9][0-9] ?:")) %>% 
+  mutate(title = str_remove(title, "CPIH MONTHLY RATE ")) %>% 
+  mutate(level = 1)
+
+cpih_mth2 <- metadata %>% 
+  filter(str_detect(title, "CPIH MONTHLY RATE [0-9][0-9]\\.[0-9] ?:")) %>% 
+  mutate(title = str_remove(title, "CPIH MONTHLY RATE ")) %>% 
+  mutate(level = 2)
+
+cpih_mth3 <- metadata %>% 
+  filter(str_detect(title, "CPIH MONTHLY RATE [0-9][0-9]\\.[0-9].[0-9] ?:")) %>% 
+  mutate(title = str_remove(title, "CPIH MONTHLY RATE ")) %>% 
+  mutate(level = 3)
+
+
+cpih_mth4 <- metadata %>% 
+  filter(str_detect(title, "CPIH MONTHLY RATE [0-9][0-9]\\.[0-9].[0-9].[0-9]")) %>% 
+  mutate(title = str_remove(title, "CPIH MONTHLY RATE ")) %>% 
+  mutate(level = 4)
+
+
+cpih_mth <- bind_rows(list(cpih_mth1, cpih_mth2, cpih_mth3, cpih_mth4)) %>% 
+            mutate(title = str_remove(title, " 2015=100")) %>% 
+            arrange(title)
+
+saveRDS(cpih_mth, here("data", "tidy", "cpih_mth_rate_cdids.rds"))
+
+
+
+
+
+# RPI average prices -----------------------------------------------------------
+
+rpi <- metadata %>% 
+  filter(str_detect(title, "RPI? ?:? ?Ave")) %>% 
+  mutate(title = str_remove(title, "RPI: Ave price - ")) %>%
+  mutate(title = str_remove(title, "RPI: Ave Price - ")) %>%
+  mutate(title = str_remove(title, "RPI: Ave Price: ")) %>%
+  mutate(title = str_remove(title, "RPI :Ave price - ")) %>%
+  mutate(title = str_remove(title, "RPI: Average [Pp]rice - ")) %>%
+  mutate(title = str_remove(title, "RPI Average [Pp]rice- ")) %>%
+  mutate(title = str_remove(title, "RPI Average [Pp]rice - ")) %>%
+  arrange(title)
+
+saveRDS(rpi, here("data", "tidy", "rpi_avg_price_cdids.rds"))
+
+
+# Contributions to CPIH annual rate -------------------------------------------
+cpih_cont_ann <- metadata %>% 
+                filter(str_detect(title, "CPIH: Contribution to all items annual rate: ")) %>% 
+                arrange(title)
+
+saveRDS(cpih_cont_ann, here("data", "tidy", "cpih_cont_ann_cdids.rds"))
+
+# Contributions to monthly change in rate -------------------------------
+cpih_cont_mth_chg <- metadata %>% 
+  filter(str_detect(title, "CPIH: Contribution to monthly change in all items index: ")) %>% 
+  arrange(title)
+
+saveRDS(cpih_cont_mth_chg, here("data", "tidy", "cpih_cont_mth_chg_cdids.rds"))
+
+# Contributions to monthly change in annual rate -------------------------------
+cpih_cont_ann_chg <- metadata %>% 
+  filter(str_detect(title, "CPIH: % points change over previous month \\(12 month rate\\): ")) %>% 
+  arrange(title)
+
+saveRDS(cpih_cont_ann_chg, here("data", "tidy", "cpih_cont_ann_chg_cdids.rds"))
+
+
