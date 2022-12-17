ddbd9c613130b6afbc36af24a1cf3ea1670b8371
David Lee <david@lee269.co.uk>
2022-12-17 15:10:52 +0000

    more shiny

--- R/app.R
+++ R/app.R
@@ -1,32 +1,49 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(here)
 
+source(here("R", "cdid_chart.R"))
 mm23 <- readRDS(here("data", "tidy", "mm23.rds"))
 data <- mm23$data
 
 ui <- fluidPage(
+  # Global options on top row
   fluidRow(
    column(2,
           selectInput("period",
-                      "Choose period",
-                      choices = c("M", "Q", "Y"),
-                      selected = "M"),
-          selectInput("series",
-                      "Choose series",
-                      choices = setNames(mm23$cpih_ann_rate_cdids$cdid, mm23$cpih_ann_rate_cdids$title)),
+                      "Frequency:",
+                      choices = list(Month =  "M", Quarter = "Q", Year =  "Y"),
+                      selected = "M")
           ),
-   column(8,
-          plotOutput("chart"))
+   column(2,
+          dateInput(inputId = "startdate",
+                    label = "Start date:",
+                    value = "2020-01-01")
+          )
   ),
+  fluidRow(
+    # Local options on left column
+    column(4,
+           selectInput("series",
+                       "Choose series",
+                       choices = setNames(mm23$cpih_ann_rate_cdids$cdid, mm23$cpih_ann_rate_cdids$title),multiple = TRUE)
+    ),
+    # Chart area in right column
+    column(8,
+           plotOutput("chart"))
+    
+  )
 )
 
 server <- function(input, output, session) {
+
   output$chart <- renderPlot({
-    data %>% 
-      filter(cdid == input$series & period == input$period) %>% 
-      ggplot() +
-      geom_line(aes(x = date, y = value))
+    x <- cdid_chart(data, 
+                    cdids = input$series, 
+                    freq = input$period, 
+                    start_date = input$startdate)
+    x
   })
 }
 
---
+++ R/cdid_chart.R
@@ -0,0 +1,16 @@
+
+cdid_chart <- function(data, cdids, freq = "M", start_date = "2020-01-01", labels){
+  if(missing(labels))(labels = cdids)
+  
+  chart <- data %>% 
+    dplyr::filter(cdid %in% cdids & value != is.na(value) & date >= start_date) %>% 
+    dplyr::filter(period == freq)  %>% 
+    ggplot2::ggplot() +
+    ggplot2::geom_line(aes(x = date, y = value, colour = cdid), size = 1) +
+    ggplot2::scale_color_discrete(breaks = cdids, labels = labels) +
+    ggplot2::scale_y_continuous(labels = scales::label_percent(scale = 1, accuracy = 0.1),
+                                breaks = scales::breaks_extended(10)) +
+    ggplot2::scale_x_date(date_labels = "%b %Y")
+  
+  return(chart)
+}
